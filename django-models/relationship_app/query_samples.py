9a71633cbf5b349e063a46d8fe94833d3bb8eb97
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-08-18 21:15:02 +0100

    add books.all

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 author_books = Book.objects.filter(name='author_name')
 
 # List all books in a library.
-library_books = Library.objects.get(name='library_name')
+library_books = Library.objects.get(name='library_name').books.all()
 
 # Retrieve the librarian for a library.
 librarian = Librarian.objects.get(name='library_name')
