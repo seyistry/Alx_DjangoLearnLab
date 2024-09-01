8020265c5b99fba250b4b8eddaa41ea80556e14d
Oluwaseyi Egunjobi <seyistry@gmial.com>
2024-09-01 17:36:12 +0100

    change class name

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Book
 
 # create forms.py
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
 	class Meta:
 		model = Book
 		fields = ['title', 'author', 'genre', 'description']
