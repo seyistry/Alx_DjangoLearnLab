d0fddc5d7bc9bf027a3537824409ae787e97e830
Oluwaseyi Egunjobi <seyistry@gmial.com>
2024-09-08 16:00:06 +0100

    swap import functions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -12,7 +12,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # from django_filters import rest_framework
 from rest_framework.filters import SearchFilter
-from rest_framework import status, generics
+from rest_framework import generics, status
 
 
 # this is a list view that returns all the books in the database
