584e5667318ee63f46e0e6980241d22a5d6ef5ab
Oluwaseyi Egunjobi <seyistry@gmial.com>
2024-09-08 16:25:26 +0100

    fix urls import statement

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ListBookView, DetailBookView, CreateBookView, UpdateBookView, DeleteBookView
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
-	path('books/', ListBookView.as_view(), name='book-list'),
-	path('books/<int:pk>/', DetailBookView.as_view(), name='book-detail'),
-	path('books/create/', CreateBookView.as_view(), name='book-create'),
-	path('books/update/<int:id>/', UpdateBookView.as_view(), name='book-update'),
-	path('books/delete/<int:id>/', DeleteBookView.as_view(), name='book-delete')
+	path('books/', ListView.as_view(), name='book-list'),
+	path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
+	path('books/create/', CreateView.as_view(), name='book-create'),
+	path('books/update/<int:id>/', UpdateView.as_view(), name='book-update'),
+	path('books/delete/<int:id>/', DeleteView.as_view(), name='book-delete')
 ]
