e4351ac06bd35c1b691c77b903a4a6703c493df9
Oluwaseyi Egunjobi <seyistry@gmial.com>
2024-09-11 19:58:59 +0100

    comment

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+		# 'USER': 'your_database_user',
+        # 'PASSWORD': 'your_database_password',
+        # 'HOST': 'localhost',  # Or the IP address of the server
+        # 'PORT': '5432', 
     }
 }
 
