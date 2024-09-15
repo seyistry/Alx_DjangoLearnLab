02ec47bce9eca95a6944ee8df1db49d3990bfddb
Oluwaseyi Egunjobi <seyistry@gmial.com>
2024-09-15 14:29:41 +0100

    remove "s"

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('posts/', views.postsListView.as_view(), name='posts'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_new'),
-    path('posts/<int:pk>', views.postDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/update/', views.postUpdateView.as_view(), name='post_edit'),
-    path('posts/<int:pk>/delete/', views.postDeleteView.as_view(), name='post_delete'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>', views.postDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/update/', views.postUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.postDeleteView.as_view(), name='post_delete'),
 
     # Custom views for registration and profile
     path('register/', views.register, name='register'),
