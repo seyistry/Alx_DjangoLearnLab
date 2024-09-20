785b990a5e31307eb45e6a090321eda7fda94374
Oluwaseyi <seyistry@gmail.com>
2024-09-20 23:29:42 +0100

    update user reg

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,18 +1,33 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['username', 'password', 'email', 'bio', 'profile_picture']
-    
-    # Ensure password is hashed
+
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        return super().create(validated_data)
+        # Create the user with the validated data
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None),
+        )
+
+        # Create an authentication token for the new user
+        Token.objects.create(user=user)
+
+        return user
+
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
